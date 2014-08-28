d8ae8104af69a0d594a440f223d2fa29d72a18c2
Ramakrishnan <ramakrishnan.v@happiestminds.com>
2014-08-28 10:43:34 +0530

    Use HTML5 speech systhesis

---
+++ js_lib/inject.js
@@ -0,0 +1,65 @@
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if(sender.tab) {
+    } else {
+      if(request.init == "speech") {
+        $("div, span, p, a")
+          .on('mousemove', selectDon)
+          .on('mouseout', deSelectDon)
+          .on('click', readText);
+        }
+    }
+  });
+
+function deSelectDon(event) {
+  $(event.target)
+    .removeClass('js-selected');
+}
+
+function selectDon(event) {
+    $(event.target)
+      .addClass('js-selected');
+}
+
+function readText(e) {
+  e.preventDefault();
+  if($(e.target).hasClass('js-selected')) {
+    $("div, span, p, a")
+      .off('click')
+      .off('mousemove');
+    if ($(e.target).children().length < 3) {
+      var text = $.trim($(e.target).text());
+      text = text.replace(/(\r\n|\n|\r|\t)/gm,"")
+      playThis(text);
+    } else {
+      alert("The selected area is too deep.")
+    }
+  }
+}
+
+function playThis(message) {
+  var blocks = message.split(' ');
+  var charCount = 0;
+  var longStr = [];
+  // Logic to split the words into blocks which will be a best fit for API
+  // TODO add a better logic to consider punctuations and group words accordingly.
+  for (i=0; i < blocks.length; i++) {
+      if ((charCount + blocks[i].length ) < 100) {
+        charCount = charCount + blocks[i].length;
+        longStr.push(blocks[i]);
+      } else {
+        i--;
+        playAudio(longStr.join(' '))
+        longStr = []
+        charCount = 0
+      }
+  }
+  if (longStr.length > 0) {
+    playAudio(longStr.join(' '))
+  }
+}
+
+function playAudio(textMessage) {
+  var msg = new SpeechSynthesisUtterance(textMessage);
+  window.speechSynthesis.speak(msg);
+}
--- js_lib/translate.js
+++ js_lib/translate.js
@@ -1,51 +1,11 @@
 $(document).ready(function() {
-        $("#speak").on('click', playThis);
-        timeOut = 0;
-        index = -1;
-        toSpeak = [];
-        vs = $('#audio_content');
-        vt = $('#vtag');
-        $(vt).on('loadedmetadata', function(e) {
-          timeOut = e.currentTarget.duration * 1000;
-          if (toSpeak.length > 0) {
-            index = index + 1;
-            if (index < toSpeak.length ) {
-              setTimeout(function() {
-                playAudio(toSpeak[index])
-              }, timeOut);
-            }
-          }
-        });
-      });
-
-      function playThis() {
-        var message = $("#content").val();
-        var blocks = message.split(' ');
-        var charCount = 0;
-        var longStr = [];
-        // Logic to split the words into blocks which will be a best fit for
-        // Google translate API
-        for (i=0; i < blocks.length; i++) {
-            if ((charCount + blocks[i].length + 1) < 100) {
-              charCount = charCount + blocks[i].length + 1;
-              longStr.push(blocks[i]);
-            } else {
-              i--;
-              toSpeak.push(longStr.join(' '))
-              longStr = []
-              charCount = 0
-            }
-        }
-        if (longStr.length > 0) {
-          toSpeak.push(longStr.join(' '))
-        }
-        playAudio(toSpeak.shift());
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if(sender.tab) {
+        playThis(request.text);
       }
-
-      function playAudio(textMessage) {
-        textMessage = escape(textMessage);
-        var inputAudio = "https://translate.google.co.in/translate_tts?ie=UTF-8&q="+ textMessage + "&tl=en&total=1&idx=0&textlen=5&client=t&prev=input";
-          vs.attr('src', inputAudio);
-          vt.get(0).load();
-          // vt.get(0).play();
-      }
+    });
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {init: "speech"});
+  });
+});
